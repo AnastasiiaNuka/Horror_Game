39e5e4c1669019deec7eea4d0233d1e2f45955a5
AnastasiiaNuka <nastianuka97@gmail.com>
2023-10-31 15:10:42 +0100

    Added scripts for jumpscare monster animation. Created laser switch script for main camera

---
+++ Assets/Scripts/monsterJumpscare.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class monsterJumpscare : MonoBehaviour
+{
+    public Animator monsterAnim;
+    public GameObject player;
+    public float jumpscareTime;
+    public string sceneName;
+    public enemyMonsterAI monsterscript;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.SetActive(false);
+            monsterscript.enabled = false;
+            monsterAnim.ResetTrigger("idle");
+            monsterAnim.ResetTrigger("walk");
+            monsterAnim.ResetTrigger("run");
+            monsterAnim.SetTrigger("jumpscare");
+            StartCoroutine(jumpscare());
+        }
+    }
+    IEnumerator jumpscare()
+    {
+        yield return new WaitForSeconds(jumpscareTime);
+        SceneManager.LoadScene(sceneName);
+    }
+}
